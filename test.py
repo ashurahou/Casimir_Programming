ecf1b7eb59d4c855097d453d7d0534b0627784f4
zhehou <z.hou@tudelft.nl>
2017-11-09 14:54:25 +0100

    docstring for surface area function

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ def circcir(r):
 	return np.pi*2*r
 
 print('Circumference of circle with radius 5 is',circcir(5))
-
+'''
 def surarea(r):
 	return np.pi*r**2
-
+'''
 print('Surface area of the cirlce with radius 5 is', surarea(5))
