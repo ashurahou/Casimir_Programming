c325bc0245bfae9d05330cb71c5ec2fbc286d532
zhehou <z.hou@tudelft.nl>
2017-11-09 13:41:03 +0100

    script file updated with function call from test.py

---
+++ script.py
@@ -0,0 +1,7 @@
+print('Hello Hell')
+print('Hello people')
+
+import test as ts
+
+print('The circumference for the circle with radius 5 is', ts.circcir(5))
+print('The surface area for the circle with radius 5 is',ts.surarea(5))
