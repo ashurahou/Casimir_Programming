3d98dd51a9fde886651ced524202bfd912d88b9a
zhehou <z.hou@tudelft.nl>
2017-11-09 13:50:37 +0100

    Add an input function for radius

--- script.py
+++ script.py
@@ -3,5 +3,8 @@ print('Hello people')
 
 import test as ts
 
-print('The circumference for the circle with radius 5 is', ts.circcir(5))
-print('The surface area for the circle with radius 5 is',ts.surarea(5))
+r =float(input('radius ='))
+print(r,type(r))
+
+print('The circumference for the circle with radius 5 is', ts.circcir(r))
+print('The surface area for the circle with radius 5 is',ts.surarea(r))
